a04b6b5d5b9ce3361d623e4f0742024e76fe6b09
ASchro1 <aaron243@revature.com>
2022-08-11 15:49:32 -0400

    Challenge 2 complete

--- src/main/java/MainDriver.java
+++ src/main/java/MainDriver.java
@@ -3,9 +3,26 @@ public class MainDriver {
     public static int quadraticEquation(int a, int b, int c){
         return (int)Math.round(((-b/(2.0*a)) + (Math.sqrt((b*b) - (4*a*c))/(2*a))));
     }
+    public static boolean isPerfect(int n){
+        int multiples = 1;
+        int sum = 0;
+        while(multiples < n){
+            if(n%multiples == 0){
+                sum = sum + multiples;
+            }
+            multiples++;
+        }
+        return (sum == n);
+    }
     public static void main(String[] args){
         System.out.println(quadraticEquation(1, 2, -3));
         System.out.println(quadraticEquation(2, -7, 3));
         System.out.println(quadraticEquation(1, -12, -28));
+        System.out.println(isPerfect(6));
+        System.out.println(isPerfect(28));
+        System.out.println(isPerfect(496));
+        System.out.println(isPerfect(12));
+        System.out.println(isPerfect(97));
+
     }
 }
